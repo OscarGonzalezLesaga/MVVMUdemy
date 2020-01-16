560593b2aed183320bd6973d730c6e13f16359a5
Oscar Gonzalez Lesaga <oscargonles@gmail.com>
2020-01-16 11:45:09 +0100

    [feat] add package and launch activity button listener

--- app/src/main/java/com/oscar/gonzalez/lesaga/mvvmudemycourse/MainActivity.kt
+++ app/src/main/java/com/oscar/gonzalez/lesaga/mvvmudemycourse/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.oscar.gonzalez.lesaga.mvvmudemycourse
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.oscar.gonzalez.lesaga.mvvmudemycourse.ui.ViewModelActivity
+import kotlinx.android.synthetic.main.activity_main.btViewModel
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        btViewModel.setOnClickListener {
+            startActivity(Intent(applicationContext, ViewModelActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/oscar/gonzalez/lesaga/mvvmudemycourse/ui/ViewModelActivity.kt
@@ -0,0 +1,13 @@
+package com.oscar.gonzalez.lesaga.mvvmudemycourse.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oscar.gonzalez.lesaga.mvvmudemycourse.R
+
+class ViewModelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_model)
+    }
+}
